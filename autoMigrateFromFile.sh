9f165e8841ba682ac8820695e860470d5a4fbbd0
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2018-09-17 17:36:47 +0200

    Update to non-bugged openstack cli

--- autoMigrateFromFile.sh
+++ autoMigrateFromFile.sh
@@ -4,7 +4,7 @@ to_node=$2
 
 while read id; do
   echo $id to $to_node
-  openstack server migrate --block-migration --live $to_node --wait $id
+  openstack server migrate --shared-migration --live $to_node --wait $id
   echo sleeping...
   sleep 3
   echo continuing...
