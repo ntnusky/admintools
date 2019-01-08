86f0becc3432628bde7a461dabbd27180459299b
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2019-01-08 13:13:28 +0100

    Add quota script for bachelor projects

---
+++ setBachelorQuota.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [[ $# -lt 1 ]]; then
+  echo "This script will set the default quota for bachelor projects"
+  echo "According to https://www.ntnu.no/wiki/display/skyhigh/Quotas"
+  echo
+  echo "Usage: $0 <project-id|project-name>"
+  exit 1
+fi
+
+project=${1}
+
+openstack project show $project &> /dev/null
+if [[ $? -eq 0 ]]; then
+  echo "Project exists..."
+  echo "Setting quotas..."
+  openstack quota set --cores 8 $project
+  openstack quota set --instances 16 $project
+  openstack quota set --ram 16384 $project
+  openstack quota set --gigabytes 40 $project
+  openstack quota set --volumes 16 $project
+
+  openstack quota show $project
+else
+  echo "No project with ID/name $project exists. Exiting"
+  exit 1
+fi
