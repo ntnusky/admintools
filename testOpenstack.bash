c75c61e1360e9dc2406bb95946e51cf948440dc6
Eigil Obrestad <eigil-git@obrestad.org>
2020-11-25 15:03:51 +0100

    Make the testscript contain installation-name in statusfiles

--- testOpenstack.bash
+++ testOpenstack.bash
@@ -2,6 +2,8 @@
 
 echo="echo [TESTOPENSTACK]"
 
+U="${OS_REGION_NAME}_${OS_PROJECT_NAME}"
+
 while [[ ! -z $1 ]]; do
   if [[ $1 == 'create' ]]; then
     create='x'
@@ -19,62 +21,62 @@ if [[ ! -z $create ]]; then
   if [[ ! -e .openstackTest.misc ]]; then
     $echo "Misc create"
     testing/misc.bash create
-    touch .openstackTest.misc
+    touch .openstackTest.${U}.misc
   fi
 
   if [[ ! -e .openstackTest.glance ]]; then
     $echo "Glance create"
     testing/glance.bash create
-    touch .openstackTest.glance
+    touch .openstackTest.${U}.glance
   fi
 
   if [[ ! -e .openstackTest.neutron ]]; then
     $echo "Neutron create"
     testing/neutron.bash create
-    touch .openstackTest.neutron
+    touch .openstackTest.${U}.neutron
   fi
 
   if [[ ! -e .openstackTest.nova ]]; then
     $echo "Nova create"
     testing/nova.bash create
-    touch .openstackTest.nova
+    touch .openstackTest.${U}.nova
   fi
 
   if [[ ! -e .openstackTest.cinder ]]; then
     $echo "Cinder create"
     testing/cinder.bash create
-    touch .openstackTest.cinder
+    touch .openstackTest.${U}.cinder
   fi
 fi
 
 if [[ ! -z $delete ]]; then
-  if [[ -e .openstackTest.cinder ]]; then
+  if [[ -e .openstackTest.${U}.cinder ]]; then
     $echo "Cinder delete"
     testing/cinder.bash delete
-    rm .openstackTest.cinder
+    rm .openstackTest.${U}.cinder
   fi
   
-  if [[ -e .openstackTest.nova ]]; then
+  if [[ -e .openstackTest.${U}.nova ]]; then
     $echo "Nova delete"
     testing/nova.bash delete
-    rm .openstackTest.nova
+    rm .openstackTest.${U}.nova
   fi
   
-  if [[ -e .openstackTest.neutron ]]; then
+  if [[ -e .openstackTest.${U}.neutron ]]; then
     $echo "Neutron delete"
     testing/neutron.bash delete
-    rm .openstackTest.neutron
+    rm .openstackTest.${U}.neutron
   fi
   
-  if [[ -e .openstackTest.glance ]]; then
+  if [[ -e .openstackTest.${U}.glance ]]; then
     $echo "Glance delete"
     testing/glance.bash delete
-    rm .openstackTest.glance
+    rm .openstackTest.${U}.glance
   fi
   
-  if [[ -e .openstackTest.misc ]]; then
+  if [[ -e .openstackTest.${U}.misc ]]; then
     $echo "Misc delete"
     testing/misc.bash delete
-    rm .openstackTest.misc
+    rm .openstackTest.${U}.misc
   fi
 fi
