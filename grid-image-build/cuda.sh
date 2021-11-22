2ce5e293a411517b17bee64b70bed7e56dffd8b4
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2021-11-22 13:08:30 +0100

    Correct PATH for CUDA 11.4

--- grid-image-build/cuda.sh
+++ grid-image-build/cuda.sh
@@ -1,3 +1,3 @@
-export PATH=/usr/local/cuda/bin:/usr/local/cuda/nsight-compute-2020.1.1${PATH:+:${PATH}}
+export PATH=/usr/local/cuda/bin:/usr/local/cuda/nsight-compute-2021.2.2${PATH:+:${PATH}}
 export CUDA_ROOT=/usr/local/cuda/bin
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
