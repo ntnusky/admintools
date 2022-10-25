d8765c90290dc865e65e6ebde88bfe39dcd6985b
Eigil Obrestad <eigil-git@obrestad.org>
2022-10-25 21:27:30 +0200

    Limits list shows invalid values for cinder

--- projectadmin/quotaCheck.sh
+++ projectadmin/quotaCheck.sh
@@ -8,13 +8,11 @@ set -e
 prereq
 need_admin
 
-while getopts i:c:v:g:r:p: option; do
+while getopts i:c:r:p: option; do
   case "${option}" in 
     i) instances=${OPTARG} ;;
     c) cores=${OPTARG} ;;
     r) ram=${OPTARG} ;;
-    v) volumes=${OPTARG} ;;
-    g) gigabytes=${OPTARG} ;;
     p) project=${OPTARG} ;;
   esac
 done
@@ -27,8 +25,6 @@ if [[ -z $project ]]; then
   echo " -i <N>: Would a new quota with N instances be valid?"
   echo " -c <N>: Would a new quota with N CPUs be valid?"
   echo " -r <N>: Would a new quota with N GB RAM be valid?"
-  echo " -v <N>: Would a new quota with N cinder volumes be valid?"
-  echo " -g <N>: Would a new quota with N GB cinder be valid?"
   exit $EXIT_MISSINGARGS
 fi
 
@@ -55,20 +51,6 @@ if [[ -z $ram || $(($totalRAMUsed/1024)) -le $ram ]]; then
   echo " - RAM (GB):  $(($totalRAMUsed/1024)) / $(($maxTotalRAMSize/1024))"
 else
   echo " * RAM (GB):  $(($totalRAMUsed/1024)) / $(($maxTotalRAMSize/1024))"
-  e=1
-fi
-
-if [[ -z $volumes || $totalVolumesUsed -le $volumes ]]; then
-  echo " - Volumes:   $totalVolumesUsed / $maxTotalVolumes"
-else
-  echo " * Volumes:   $totalVolumesUsed / $maxTotalVolumes"
-  e=1
-fi
-
-if [[ -z $gigabytes || $totalGigabytesUsed -le $gigabytes ]]; then
-  echo " - Volume GB: $totalGigabytesUsed / $maxTotalVolumeGigabytes"
-else
-  echo " * Volume GB: $totalGigabytesUsed / $maxTotalVolumeGigabytes"
   e=1
 fi
 
