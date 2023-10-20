0c7a70eb0a1e425c640c6469e9ca128b8dd189c1
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2023-10-20 10:28:29 +0200

    Add VM creator to the output

--- getVMowner.sh
+++ getVMowner.sh
@@ -14,24 +14,29 @@ fi
 vm=$1
 oscmd=$(which openstack)
 
-vm_data=$($oscmd server show -f json -c project_id -c properties -c name "$vm")
+vm_data=$($oscmd server show -f json -c project_id -c properties -c name -c user_id "$vm")
 project=$(echo "$vm_data" | jq -r '.project_id')
 properties=$(echo "$vm_data" | jq -r '.properties')
 vm_name=$(echo "$vm_data" | jq -r '.name' )
+user=$(echo "$vm_data" | jq -r '.user_id' )
 projectname=$($oscmd project show -f value -c name "$project")
+creator=$($oscmd user show -f value -c name "$user")
 
 box "User info about VM: $vm ($vm_name)"
 echo "Project: $projectname"
+echo "User that created the VM: $creator"
 
 if [ "$properties" != "{}" ]; then
   echo "VM custom properties: $properties"
 else
+  box "Current project members"
   $oscmd role assignment list -f csv -c User -c  Group --project "$project" --names | tr -d '"' | grep 'NTNU' | uniq | while read -r line; do
     username=$(echo "$line" | cut -d',' -f1 | cut -d'@' -f1)
     group=$(echo "$line" | cut -d',' -f2 | cut -d '@' -f1)
 
     if [ -n "$username" ]; then
       userdetails=$($oscmd user show -f value -c id -c email --domain NTNU "$username")
+      id=$(echo $userdetails | cut -d' ' -f2)
       if [[ $userdetails =~ @ ]]; then
         mail=$(echo $userdetails | cut -d' ' -f1)
       # No email registered. Assume student with RESERVE_PUBLISH
