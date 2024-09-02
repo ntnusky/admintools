240f8fe65599ff6ca9f8b9e1e48edb5a35365ea3
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2024-09-02 10:12:34 +0200

    Script to mark a project with the notified_delete tag

---
+++ projectadmin/markProjectAsNotifiedDelete.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+. $(dirname $0)/../common.sh
+. $(dirname $0)/functions.sh
+
+prereq
+need_admin
+
+project=$1
+
+if openstack project show $project &> /dev/null; then
+  openstack project set --tag notified_delete $project
+  echo "Project $project now has the tag 'notified_delete' set"
+else
+  echo "Project $project does not exist"
+  exit $EXIT_CONFIGERROR
+fi
