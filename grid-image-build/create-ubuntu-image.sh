31a92f6d29bcf3b4d47b9a37d090c518e9d7c9a0
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2025-06-27 12:55:15 +0200

    Changes needed for Ubuntu 24.04

--- grid-image-build/create-ubuntu-image.sh
+++ grid-image-build/create-ubuntu-image.sh
@@ -6,12 +6,12 @@ rawimage='ubuntu-grid.raw'
 echo "Copying image"
 cp $orgimage $image
 echo "Adding deb-file for CUDA repo"
-virt-customize -a $image --run-command 'wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb -O /tmp/cuda-keyring.deb'
+virt-customize -a $image --run-command 'wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb -O /tmp/cuda-keyring.deb'
 virt-customize -a $image --run-command 'dpkg -i /tmp/cuda-keyring.deb'
 echo "Upgrading packages"
 virt-customize -a $image --update
 echo "Installing packages"
-virt-customize -a $image --install build-essential,dkms,libxml2-utils,libglvnd-core-dev,cuda-toolkit-12-2,libcudnn8,libcudnn8-dev
+virt-customize -a $image --install build-essential,dkms,libxml2-utils,libglvnd-core-dev,cuda-toolkit-12-9,cuda-compat-12-9,libcudnn9-cuda-12,libcudnn9-dev-cuda-12
 echo "Setting correct timezone"
 virt-customize -a $image --timezone Europe/Oslo
 echo "Installing GRID-script"
