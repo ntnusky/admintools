a01a3af171dbea482d667069c8804451169f0c54
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2026-01-06 12:27:54 +0100

    neutron-kommandoen er så deprecated den. Og den nye kommandoen må ha ID til nettet, ikke navnet

--- projectadmin/createProjectDCSG2003.sh
+++ projectadmin/createProjectDCSG2003.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 parent="DCSG2003_Workers"
-net="dcsg2003"
+net="35fe8b76-cc31-4b48-b3f0-5c48eef8d289"
 
 if [[ $# -lt 3 ]]; then
   echo "This script assignes users to projects; and if the project"
@@ -57,7 +57,7 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
 
   if [[ $exists -eq 0 ]]; then
     projectID=$(openstack project show $projectName -f value -c id) 2> /dev/null
-    $cmd neutron rbac-create --target-tenant $projectID \
+    $cmd openstack network rbac create --target-project $projectID --target-project-domain NTNU \
         --action access_as_shared --type network $net
   fi
   echo " -- DONE adding $username to $projectName"
