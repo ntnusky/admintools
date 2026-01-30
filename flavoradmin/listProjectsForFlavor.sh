ec4996cc4ee9bf4fdb4ee78faafa80e3f718db0b
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2026-01-30 10:37:26 +0100

    Print antall aktive VMer av gitt flavor per prosjekt

--- flavoradmin/listProjectsForFlavor.sh
+++ flavoradmin/listProjectsForFlavor.sh
@@ -12,7 +12,9 @@ projects=$(openstack flavor show $flavor -f json -c access_project_ids 2> /dev/n
 if [[ -n "$projects" ]]; then
   echo "Project with access to the $flavor flavor"
   for project in $projects; do
-    openstack project show -f value -c name $project
+    project_name=$(openstack project show -f value -c name $project)
+    num_servers=$(openstack server list -f value --project $project --flavor $flavor | grep -v SHELVED | wc -l)
+    echo -e "$project_name - $num_servers active servers"
   done
 else
   echo "The flavor does not exist, or no projects has access to it"
