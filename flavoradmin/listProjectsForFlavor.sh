8edb6e1db2619e74f54d2b37dc9932fe920f5190
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2026-01-30 10:23:44 +0100

    Script for å liste ut prosjekt som har tilgang til en gitt flavor

---
+++ flavoradmin/listProjectsForFlavor.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+. $(dirname $0)/../common.sh
+
+need_admin
+prereq
+
+flavor=$1
+
+projects=$(openstack flavor show $flavor -f json -c access_project_ids 2> /dev/null | jq -r .access_project_ids[])
+
+if [[ -n "$projects" ]]; then
+  echo "Project with access to the $flavor flavor"
+  for project in $projects; do
+    openstack project show -f value -c name $project
+  done
+else
+  echo "The flavor does not exist, or no projects has access to it"
+fi
