93c5166df76e21f277755f2ecb7d93db94df57b2
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2026-05-11 15:21:07 +0200

    Add the cuda-compat folder to LD_LIBRARY_PATH

--- grid-image-build/cuda.sh
+++ grid-image-build/cuda.sh
@@ -1,3 +1,4 @@
 export PATH=/usr/local/cuda/bin${PATH:+:${PATH}}
 export CUDA_ROOT=/usr/local/cuda/bin
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda/compat${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
